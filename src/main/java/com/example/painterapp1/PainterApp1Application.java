8ba459f81e892631ff292c0314b632823aa03602
Islam Momukeev <islambek.momukeev_2026@ucentralasia.org>
2023-05-08 03:04:13 +0600

    Asd

--- src/main/java/com/example/painterapp1/PainterApp1Application.java
+++ src/main/java/com/example/painterapp1/PainterApp1Application.java
@@ -12,7 +12,7 @@ public class PainterApp1Application extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PainterApp1Application.class.getResource("PainterApp1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("PainterApp");
         stage.setScene(scene);
         stage.show();
     }
